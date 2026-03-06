39ac093e7cc10c28beb9177ccd16c38db9d14828
Fethulmubin Ahmed <fetihul@gmail.com>
2026-03-07 00:42:20 +0300

    feat(faqs): schema.ts implemented for faqs

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -2,11 +2,13 @@ import { Hono } from "hono";
 import auth from "./auth";
 import post from "./posts";
 import comment from "./comments";
+import faq from "./faqs";
 
 const v1 = new Hono();
 
 v1.route('/auth', auth);
 v1.route('/posts', post);
 v1.route('/comments', comment)
+v1.route('/faqs', faq);
 
 export default v1;
